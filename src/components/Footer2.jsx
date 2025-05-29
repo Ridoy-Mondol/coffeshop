5b32a3b262274310ef080a22fc2ca36aeeb0a96d
Ridoy Mondol <ridoymondol140@gmail.com>
2025-05-29 16:18:42 +0600

    update name

--- src/components/Footer2.jsx
+++ src/components/Footer2.jsx
@@ -1,7 +1,7 @@
 const Footer2 = () => {
     return (
         <footer className="footer-2">
-           <span>Copyright &copy; : {new Date().getFullYear()} | All Rights Reserved By Md. Ridoy Mondol</span>
+           <span>Copyright &copy; : {new Date().getFullYear()} | All Rights Reserved By Md. Ahatashamul Islam Mondol</span>
         </footer>
     )
 }
